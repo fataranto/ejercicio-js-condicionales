b777e6ea871600972d49d0582772b561dc392f52
fataranto <fataranto@gmail.com>
2022-05-13 17:35:46 +0200

    hasta ejercicio 5

--- ex5.js
+++ ex5.js
@@ -5,8 +5,13 @@
  * 2. En caso contrario, mostramos por el temrinal "El número es menor de 5"
  */
 
-let numero = 5;
+let numero = 2;
 
+if (numero >= 5) {
+    console.log("El número es mayor de 5");
+} else {
+    console.log("El número es menor de 5");
+}
 // AQUÍ TU CÓDIGO. Necesitas codificar una estructura if...else. Mira los ejemplos anteriores. Cambia el valor de la variable "numero" para comprobar que has implementado bien la solución https://www.w3schools.com/js/js_if_else.asp
 
 
